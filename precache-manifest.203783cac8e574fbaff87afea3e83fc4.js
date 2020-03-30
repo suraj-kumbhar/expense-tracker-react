5dd47d909a0f1b7d82839cf9be0584d9ce8068c2
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-03-30 16:31:24 +0530

    Updates

---
+++ precache-manifest.203783cac8e574fbaff87afea3e83fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0001b7458a86edeea42c8274fad8dc96",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "ee91e390902f58c954d3",
+    "url": "/expense-tracker-react/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "4486e51f806333b2cd0e",
+    "url": "/expense-tracker-react/static/js/2.f152ab3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.f152ab3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee91e390902f58c954d3",
+    "url": "/expense-tracker-react/static/js/main.26b172e8.chunk.js"
+  },
+  {
+    "revision": "862869cc79fa6dc735cc",
+    "url": "/expense-tracker-react/static/js/runtime-main.bdd3deb4.js"
+  }
+]);
